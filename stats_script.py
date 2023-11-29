773ea0371410dd9931f84351fa601fd667c565bb
Rudy Colombel <rudy.colombel@iia-formation.fr>
2023-11-29 16:17:27 +0100

    Modification pour la nouvelle version de Linux

--- stats_script.py
+++ stats_script.py
@@ -6,3 +6,6 @@ memory = psutil.virtual_memory()
 # Print the memory usage details
 print("Total Memory: {:.2f} GB".format(memory.total / (1024 ** 3)))
 print("Available Memory: {:.2f} GB".format(memory.available / (1024 ** 
+                                                               
+                                                               # Get the memory usage information
+memory = psutil.virtualMemory()
